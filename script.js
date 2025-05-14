e0d1847cb3bf63792cfe6133260fc27d75fbe8cb
arizig <arizig13@gmail.com>
2025-05-14 11:06:22 +0200

    script.js

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ function generateAmount() {
 
 let donors = [];
 let sortAsc = true;
+
+fetch('https://randomuser.me/api/?results=20')
+  .then(res => res.json())
+  .then(data => {
+    donors = data.results.map(user => ({
+      name: `${user.name.first} ${user.name.last}`,
+      firstName: user.name.first,
+      gender: user.gender,
+      phone: user.phone,
+      location: `${user.location.city}, ${user.location.country}`,
+      picture: user.picture.medium,
+      amount: parseFloat(generateAmount())
+    }));
+    displayDonors(donors);
+  });
